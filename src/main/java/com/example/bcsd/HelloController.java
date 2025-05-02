097e9b68fae2b1a2db48926ec74aa976a9f1a28e
jo wonhee <wonhee6985@gmail.com>
2025-05-02 14:47:39 +0900

    HelloController와 hello.html 수정 반영

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -1,20 +1,30 @@
 package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
 
-    @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!!!!!";
-    }
 
-    @GetMapping("/hello2")
-    public String hello2() {
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam(name="name" , required=false, defaultValue = "조원희") String name, Model model) {
+        model.addAttribute("name", name);
         return "hello";
+
+    }
+    @GetMapping("/json")
+    @ResponseBody
+    public Map<String, Object> json() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("age", 24);
+        result.put("name", "조원희");
+        return result;
     }
 }

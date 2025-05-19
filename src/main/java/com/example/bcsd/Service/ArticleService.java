544550c458e903b2f9a9d0a7c1be96ef422b54f7
jo wonhee <wonhee6985@gmail.com>
2025-05-19 21:44:41 +0900

    [조원희_BackEnd] 7주차 과제 끝

--- src/main/java/com/example/bcsd/Service/ArticleService.java
+++ src/main/java/com/example/bcsd/Service/ArticleService.java
@@ -5,11 +5,13 @@ import com.example.bcsd.Model.Article;
 import com.example.bcsd.repository.ArticleRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class ArticleService {
     private final ArticleRepository articleRepository;
@@ -22,6 +24,7 @@ public class ArticleService {
     public List<Article> getArticlesByBoardId(Long board_id) {
         return articleRepository.findByBoardId(board_id);
     }
+
     public ResponseEntity<Article> getArticleById(Long id) {
         Article article = articleRepository.findById(id);
         if (article == null) {
@@ -29,10 +32,14 @@ public class ArticleService {
         }
         return ResponseEntity.ok(article);
     }
-    public ResponseEntity<Article> createArticle( Article article) {
+
+    @Transactional
+    public ResponseEntity<Article> createArticle(Article article) {
         articleRepository.insert(article);
         return ResponseEntity.ok(article);
     }
+
+    @Transactional
     public ResponseEntity<Article> updateArticle(Article article, Long id) {
         Article articles = articleRepository.findById(id);
         if (articles == null) {
@@ -45,8 +52,10 @@ public class ArticleService {
         // 변경된 정보를 다시 조회해서 돌려줄 수도 있습니다.
         return ResponseEntity.ok(articleRepository.findById(id));
     }
+
+    @Transactional
     public void deleteArticle(Long id) {
-        if(!articleRepository.delete(id)) {
+        if (!articleRepository.delete(id)) {
             throw new NullPointerException("해당 id가 존재하지 않습니다.");
         }
     }
